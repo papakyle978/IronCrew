05fa1f154abe31033f01caa44450f8601dd52ce1
papakyle978 <papakyle978@gmail.com>
2026-08-08 21:14:11 -0400

    ll

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import app from '../server';
+// FIX: Explicitly add the .ts extension so Node's ESM loader can resolve it!
+import app from '../server.ts'; 
 
 export default app;
